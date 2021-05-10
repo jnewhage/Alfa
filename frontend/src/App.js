8f08a8d6fc30782e36b8811631509e606a53349a
Newhagen <jnewhage@umd.edu>
2021-05-10 18:15:36 -0400

    stuff

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -241,18 +241,18 @@ class App extends Component {
       <div>
         <BrowserRouter>
           <div className="App">
-            <Route exact path="/" component={Login2} />
-            <Route exact path="/signup.js" component={Signup} />
-            <Route exact path="/signup" component={Signup} />
-            <Route exact path="/index.js" component={Login2} />
-            <Route exact path="/login.js" component={Login2} />
-            <Route exact path="/login" component={Login2} />
-            <Route exact path="/home.js" component={Home} />
-            <Route exact path="/alfa_parts.js" component={AlfaParts} />
-            <Route exact path="/GTV6_parts.js" component={GTV6Parts} />
-            <Route exact path="/milano_parts.js" component={MilanoParts} />
-            <Route exact path="/mechanics.js" component={Mechanics} />
-            <Route exact path="/engine.js" component={Engine} />
+            <Route path="/" component={Login2} />
+            <Route path="/signup.js" component={Signup} />
+            <Route path="/signup" component={Signup} />
+            <Route path="/index.js" component={Login2} />
+            <Route path="/login.js" component={Login2} />
+            <Route path="/login" component={Login2} />
+            <Route path="/home.js" component={Home} />
+            <Route path="/alfa_parts.js" component={AlfaParts} />
+            <Route path="/GTV6_parts.js" component={GTV6Parts} />
+            <Route path="/milano_parts.js" component={MilanoParts} />
+            <Route path="/mechanics.js" component={Mechanics} />
+            <Route path="/engine.js" component={Engine} />
             <Route path="/checkout.js" component={Checkout} />
             <Route path="/electrical.js" component={Electrical} />
             <Route path="/brakes.js" component={Brakes} />
@@ -261,7 +261,7 @@ class App extends Component {
             <Route path="/cart.js" component={Cart} />
             <Route path="/electrical" component={Electrical} />
             <Route path="/brakes" component={Brakes} />
-            <Route exact path="/engine" component={Engine} />
+            <Route path="/engine" component={Engine} />
             <Route path="/checkout" component={Checkout} />
             <Route path="/cart" component={Cart} />
             <Route path="/tweets.js" component={Twitter} />
